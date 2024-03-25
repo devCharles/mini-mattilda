0febb4c84ca6a91e895ef67b6aae7fe72185aacb
DevCharles <charlesipn@gmail.com>
2024-03-25 00:17:17 -0600

    chore: remove version prefix

--- backend/app/rest/app.py
+++ backend/app/rest/app.py
@@ -13,4 +13,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(rest_router, prefix="/v1")
+app.include_router(rest_router)
--- backend/app/rest/router.py
+++ backend/app/rest/router.py
@@ -2,20 +2,20 @@ from email import message
 
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
-from resources.invoices import controller as invoices_router
-from resources.schools import controller as schools_router
-from resources.students import controller as students_router
+from resources.schools.controller import router as schools_router
+from resources.invoices.controller import router as invoices_router
+from resources.students.controller import router as students_router
 
 router = APIRouter()
 
-router.include_router(schools_router.router, prefix="/schools")
-router.include_router(students_router.router, prefix="/students")
-router.include_router(invoices_router.router, prefix="/invoices")
+router.include_router(schools_router, prefix="/schools")
+router.include_router(students_router, prefix="/students")
+router.include_router(invoices_router, prefix="/invoices")
 
 
 @router.get("/", name="Root", tags=["Root"])
 def root():
     return JSONResponse(
-        content={message: "Mini Mattilda API"},
+        content={"message": "Mini Mattilda API"},
         status_code=200,
     )
